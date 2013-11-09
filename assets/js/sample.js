33211b0dba94a2756573a8248219347dd086ae7f
Kawandeep Virdee <whichlight@gmail.com>
2013-11-09 16:02:51 -0500

    added sample class

---
+++ assets/js/sample.js
@@ -0,0 +1,26 @@
+function Sample(url){
+  this.url = url;
+  this.start;
+  this.stop;
+  this.duration;
+  this.key;
+  this.player; //the associated player obj w the iframe
+  this.init();
+  this.id;
+}
+
+Sample.prototype.init(){
+  //create the iframe
+
+  //create the player
+
+}
+
+Sample.prototype.play = function(){
+
+}
+
+//getting and setting from UI
+Sample.prototype.update = function(){
+
+}
