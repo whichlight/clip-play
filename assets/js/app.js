df761dd6400d55d99540d7d769075955681f3093
Kawandeep Virdee <whichlight@gmail.com>
2013-11-09 17:51:15 -0500

    url to media frame

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,15 +2,16 @@
 	var App = function() {
 		this.initialize = function() {
 			var sample_editor = new SampleEditor();
-			
+
 			sample_editor.initialize();
 		};
-		
-		
+
+
 		return this;
 	};
-	
-	
+
+
 	var app = new App();
 	app.initialize();
 })(jQuery);
+$.embedly.defaults.key = 'e2dcb7bae5a443bfbb5f726daf05549f';
--- assets/js/sample.js
+++ assets/js/sample.js
@@ -5,15 +5,28 @@ function Sample(url){
   this.duration;
   this.key;
   this.player; //the associated player obj w the iframe
-  this.init();
   this.id;
+  this.iframe;
+  this.init();
 }
 
-Sample.prototype.init(){
-  //create the iframe
-
-  //create the player
-
+Sample.prototype.init = function(){
+  var that = this;
+  $.embedly.oembed(this.url).done(function(results){
+    var BASE_IFRAME = "http://cdn.embedly.com/widgets/media.html";
+    var data = results[0];
+    var f= data.html;
+    var src = $(f)[0].src;
+    var schema = data.provider_name.toLowerCase();
+    var iframe_src= BASE_IFRAME +"?schema="+schema+"&type=text%2Fhtml&html="+src;
+    var iframe = $('<iframe/>', {
+      src: iframe_src
+    });
+    $('body').append(iframe[0]);
+    var player = new OP.Player(iframe[0]);
+    that.player = player;
+    that.iframe = iframe;
+  });
 }
 
 Sample.prototype.play = function(){
