836f1fc06c58d7d97e31387026eedc6902267fea
Teddy Wing <ted@teddywing.com>
2013-11-09 20:39:06 -0500

    Initialise drag handles on sample add
    When a new sample is added, initialise the drag handles so they are
    draggable.
    
    Also remove initial sample from markup.

--- assets/js/views/sample.js
+++ assets/js/views/sample.js
@@ -1,3 +1,26 @@
 ClipPlay.Views.Sample = Marionette.ItemView.extend({
-	template: '#sample-view-template'
+	template: '#sample-view-template',
+	
+	onRender: function() {
+		this.initialize_clip_drag_handles();
+	},
+	
+	
+	initialize_clip_drag_handles: function() {
+		var $clip_line = this.$('.js-clip-line');
+		
+		$clip_line.draggable({
+			axis: 'x',
+			handle: '.js-drag-handle',
+			scroll: false,
+			containment: $clip_line.parents('.progress-bar')
+		});
+		
+		$clip_line.on('dragstop', this.on_clip_drag_stop);
+	},
+	
+	
+	on_clip_drag_stop: function(e, ui) {
+		console.log('booyakacha');
+	}
 });
