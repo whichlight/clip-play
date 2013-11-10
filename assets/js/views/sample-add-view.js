73ebf518510691539cc691c2ae89aa12bbe9ee81
Teddy Wing <ted@teddywing.com>
2013-11-09 20:22:49 -0500

    Sample add: fix some bugs with adding
    * Don't add to collection if the input field is empty
    * Clear input field after adding

--- assets/js/views/sample-add-view.js
+++ assets/js/views/sample-add-view.js
@@ -5,12 +5,25 @@ ClipPlay.Views.SampleAddView = Marionette.View.extend({
 	
 	initialize: function(options) {
 		this.setElement(options.el);
+		this.$input = this.$('.js-sample-url');
+	},
+	
+	add_sample_to_collection: function() {
+		this.collection.add({
+			url: this.$input.val()
+		});
+	},
+	
+	clear_input: function() {
+		this.$input.val('');
 	},
 	
 	on_sample_add: function() {
-		this.collection.add({
-			url: this.$('.js-sample-url').val()
-		});
+		if (this.$input.val()) {
+			this.add_sample_to_collection();
+			
+			this.clear_input();
+		}
 		
 		return false;
 	}
