c38a842abc7e7e31ba22c8a34f731958ff265b5d
Kawandeep Virdee <whichlight@gmail.com>
2013-11-10 16:16:07 -0500

    fixed paths

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,15 +14,14 @@ ClipPlay.App.addInitializer(function(options) {
     //just to have something loaded for debugging
     ClipPlay.Config = {};
     ClipPlay.Config['key_defaults'] = ['a','s','d','f','g','h','j','k','l'];
+
     samples_collection.add({url:"http://www.youtube.com/watch?v=onRk0sjSgFU"});
     samples_collection.add({url:"https://vimeo.com/18150336"});
     samples_collection.add({url:"http://www.youtube.com/watch?v=8hP9D6kZseM"});
     samples_collection.add({url:"http://www.youtube.com/watch?v=QH2-TGUlwu4"});
-
     samples_collection.add({url:"http://www.youtube.com/watch?v=eau1qHxI-8w"});
-
     samples_collection.add({url:"http://www.youtube.com/watch?v=A2zKARkpDW4"});
-
+    samples_collection.add({url:"http://www.youtube.com/watch?v=ahTyGckSJqw"});
 
 });
 
