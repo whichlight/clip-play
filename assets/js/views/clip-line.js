a0b372e4e3268f478963d43ccf40b5cee1f6e082
Teddy Wing <ted@teddywing.com>
2013-11-10 01:56:55 -0500

    Add time values to the drag handles
    Time in mm:ss appears on the drag handles. Drag them around and the
    time changes accordingly.
    
    NOTE: this version is kind of borked because for some reason in my
          testing the start of the video displayed as 24 seconds.

--- assets/js/views/clip-line.js
+++ assets/js/views/clip-line.js
@@ -6,6 +6,10 @@ ClipPlay.Views.ClipLine = Marionette.View.extend({
 		
 		this.type = options.type;
 		
+		this.listenTo(this.model, 'change:duration', this.initialize_time_values);
+		var start_or_stop = this.type;
+		this.listenTo(this.model, 'change:' + start_or_stop, this.render_time);
+		
 		this.initialize_drag_handles();
 	},
 	
@@ -19,9 +23,30 @@ ClipPlay.Views.ClipLine = Marionette.View.extend({
 		});
 		
 		var that = this;
-		this.$el.on('dragstop', function(e, ui) {
-			that.on_clip_drag_stop(e, ui);
+		
+		this.$el.on('drag', function(e, ui) {
+			that.on_clip_drag(e, ui);
 		});
+	},
+	
+	
+	initialize_time_values: function() {
+		this.set_seek_point();
+		this.render_time();
+	},
+	
+	
+	render_time: function() {
+		var time;
+		if (this.type === 'start') {
+			time = this.model.start_in_minutes_and_seconds();
+		}
+		else if (this.type === 'stop') {
+			time = this.model.stop_in_minutes_and_seconds();
+		}
+		
+		var time_string = time.minutes + ':' + time.seconds;
+		this.$('.js-time').text(time_string);
 	},
 	
 	
@@ -52,7 +77,12 @@ ClipPlay.Views.ClipLine = Marionette.View.extend({
 	},
 	
 	
-	on_clip_drag_stop: function(e, ui) {
+	set_seek_point: function() {
 		this.model.set(this.type, this.seek_point());
+	},
+	
+	
+	on_clip_drag: function(e, ui) {
+		this.set_seek_point();
 	}
 });
